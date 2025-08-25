f89571a652eedcefc97dffa6146d5519e6e52f74
chengcai liu <chengcailiu@buaa.edu.cn>
2025-08-26 01:04:55 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,8 +3,30 @@ import torch.nn as nn
 from torch.nn import functional as F
 import torchvision
 from torchvision.models.video import Swin3D_B_Weights
-from safetensors.torch import load_file
 import warnings
+warnings.filterwarnings("ignore")
+import argparse
+import os
+import tempfile
+import urllib.request
+
+def load_safetensors_from_path_or_url(path_or_url: str):
+
+    if path_or_url.startswith("http://") or path_or_url.startswith("https://"):
+        # 下载到临时文件
+        tmpdir = tempfile.mkdtemp()
+        filename = os.path.basename(path_or_url)
+        local_path = os.path.join(tmpdir, filename)
+        print(f"Downloading pretrained weights from {path_or_url} ...")
+        urllib.request.urlretrieve(path_or_url, local_path)
+        print(f"Downloaded to {local_path}")
+        return torch.load(local_path, map_location="cpu")
+    else:
+        # 本地文件
+        if not os.path.exists(path_or_url):
+            raise FileNotFoundError(f"Pretrained weight file not found: {path_or_url}")
+        return torch.load(path_or_url, map_location="cpu")
+
 
 class ModelforPretrain(nn.Module):
     def __init__(self, args=None):
@@ -12,31 +34,67 @@ class ModelforPretrain(nn.Module):
         weights=Swin3D_B_Weights.KINETICS400_IMAGENET22K_V1
         self.model =  torchvision.models.video.swin3d_b(num_classes=400, weights=weights)
         self.model.head = nn.Identity()
+        self.args = args
             
-        if args:
-            self.args = args
-            if args.my_pretrained:
-                print("load pretrained model")
-                new_dict = {}
-                ckpt0 = load_file(self.args.my_pretrained)
-                for k,v in ckpt0.items():
-                    if 'vision_encoder.model.' in k:
-                        k = k.replace('vision_encoder.model.','')
-                        new_dict[k] = v
-                    else:
-                        pass
+        
+        if args.pretrained:
                 
-                newdict2 = {}
-                for k,v in self.model.state_dict().items():
-                    if k in new_dict.keys():
-                        newdict2[k] = new_dict[k]
-                    else:
-                        newdict2[k] = v
-                        print(k,"not in self.model.state_dict().keys()")
-                self.model.load_state_dict(newdict2, strict=True)
+            ckpt0 = load_safetensors_from_path_or_url(self.args.pretrained)
+                
+            self.model.load_state_dict(ckpt0, strict=True)
 
     def forward(self, x):
       
         x = self.model(x)
         
         return x
+
+
+
+
+class ModelforExtractFea(nn.Module):
+    def __init__(self, num_classes=2, args=None):
+        super(ModelforExtractFea, self).__init__()
+        self.args = args
+        self.model =  torchvision.models.video.swin3d_b(num_classes=400)
+        ## 去掉model中的head这个key
+        self.model.head = nn.Identity()
+        self.head = nn.Linear(in_features=400, out_features=num_classes, bias=True)
+        
+
+        if args.pretrained:
+
+            ckpt0 = load_safetensors_from_path_or_url(self.args.pretrained)
+            self.model.load_state_dict(ckpt0, strict=True)
+            print(f"load pretrained model done from {self.args.pretrained}")
+
+
+
+    def forward(self, x):
+        
+        x = self.model(x)
+
+        if self.args.save_feature:
+            fea = x.detach().clone()
+            return fea
+        else:
+            x = self.head(x)
+            return x
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    ## pretrained model
+    parser.add_argument('--pretrained', type=str, default='/data1/lcc/log/CLIP/downstream_task/20241219mae2/10上传github/model.pt', help='pretrained model path')
+    parser.add_argument('--save_feature', type=bool, default=True, help='save feature')
+    args = parser.parse_args()
+
+
+    # model = ModelforExtractFea(args=args)
+    model = ModelforPretrain(args=args)
+    x = torch.randn(1, 3, 48, 256, 256)
+    out = model(x)
+    print(out.shape)
+
+
+
